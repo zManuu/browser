bef1d5c8f8c0d1c79e33f6ffedb1b7be669d5f54
zManuu <60520810+zManuu@users.noreply.github.com>
2023-10-10 21:08:48 +0200

    removed electron import from renderer

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,13 +1,3 @@
 <template>
   <router-view :key="$route.fullPath" />
 </template>
-<script lang="ts">
-import { is } from '@electron-toolkit/utils'
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  mounted() {
-    if (!is.dev) this.$router.push('/')
-  }
-})
-</script>
